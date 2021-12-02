174349206459ed4e2a5db84e4e9270e7f2957034
Caleb Smith <caleb@calebsmith.net>
2021-12-02 09:06:23 +0000

    day2 progress

--- day2/day2.go
+++ day2/day2.go
@@ -1,11 +1,58 @@
 package main
 
 import (
-	"fmt"
+	"bufio"
+	"log"
+	"os"
+	"strconv"
 	"strings"
 )
 
+type instruction struct {
+	direction string
+	magnitude int
+}
+
 func main() {
-	fmt.Println("hello world")
-	fmt.Println(strings.Split("hello world", " "))
+
+	file, err := os.Open("day2.input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	instructions := make([]instruction, 0)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() { // for every line in the file
+		instructionText := scanner.Text()
+		instructionSplit := strings.Split(instructionText, " ")
+		direction := instructionSplit[0]
+		magnitude, _ := strconv.Atoi(instructionSplit[1])
+
+		instructions = append(instructions, instruction{direction, magnitude})
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// parse instructions
+	horizontal := 0
+	vertical := 0
+
+	for _, currentInstruction := range instructions {
+		if currentInstruction.direction == "forward" {
+			horizontal = horizontal + currentInstruction.magnitude
+		}
+		if currentInstruction.direction == "up" {
+			vertical = vertical + currentInstruction.magnitude
+		}
+		if currentInstruction.direction == "down" {
+			vertical = vertical + currentInstruction.magnitude
+		}
+	}
+
+	log.Println(horizontal)
+	log.Println(vertical)
+
 }

97a2b31de24cf99782240c87d7e980888702fa70
Caleb Smith <caleb@calebsmith.net>
2021-12-02 09:19:36 +0000

    day 2 part 2, I think

--- day2/day2.go
+++ day2/day2.go
@@ -15,7 +15,7 @@ type instruction struct {
 
 func main() {
 
-	file, err := os.Open("day2.input.txt")
+	file, err := os.Open("day2.demoinput.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -36,7 +36,22 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// parse instructions
+	horizontal, vertical := parseInstructionsPartOne(instructions, horizontal, vertical)
+
+	log.Println("Horizontal Position:", horizontal)
+	log.Println("Vertical Position:", vertical)
+	log.Println("Answer for Day 2, Part 1:", horizontal*vertical)
+
+	horizontal2, vertical2, aim := parseInstructionsPartTwo(instructions, horizontal, vertical)
+
+	log.Println("Horizontal Position:", horizontal2)
+	log.Println("Vertical Position:", vertical2)
+	log.Println("Aim:", aim)
+	log.Println("Answer for Day 2, Part 1:", horizontal2*vertical2)
+
+}
+
+func parseInstructionsPartOne(instructions []instruction) (int, int) {
 	horizontal := 0
 	vertical := 0
 
@@ -51,9 +66,25 @@ func main() {
 			vertical = vertical + currentInstruction.magnitude
 		}
 	}
+	return horizontal, vertical
+}
 
-	log.Println("Horizontal Position:", horizontal)
-	log.Println("Vertical Position:", vertical)
-	log.Println("Answer for Day 2, Part 1:", horizontal*vertical)
+func parseInstructionsPartTwo(instructions []instruction) (int, int, int) {
+	horizontal := 0
+	vertical := 0
+	aim := 0
 
+	for _, currentInstruction := range instructions {
+		if currentInstruction.direction == "forward" {
+			horizontal = horizontal + currentInstruction.magnitude
+			vertical = vertical + (currentInstruction.magnitude * aim)
+		}
+		if currentInstruction.direction == "up" {
+			aim = aim - currentInstruction.magnitude
+		}
+		if currentInstruction.direction == "down" {
+			aim = aim + currentInstruction.magnitude
+		}
+	}
+	return horizontal, vertical, aim
 }

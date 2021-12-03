818699769368154d460c3de54c7633796bd87e9b
Caleb Smith <caleb@calebsmith.net>
2021-12-03 06:23:48 +0000

    saving my work before i inevitably lose it all

--- day3/day3.go
+++ day3/day3.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"log"
 	"os"
+	//"strconv"
 )
 
 func main() {
@@ -24,21 +25,52 @@ func main() {
 		log.Fatal(err)
 	}
 
-	log.Println(input)
-
-}
-func gamma(input []string) int {
-	zeroes := make([]int, 5, 5)
-	ones := make([]int, 5, 5)
+	zeroes := make([]int, 5)
+	ones := make([]int, 5)
 
 	for _, entry := range input {
 		for i, bit := range entry {
-			if bit == 48 {
+			if bit == '0' {
 				zeroes[i]++
 			}
-			if bit == 49 {
+			if bit == '1' {
 				ones[i]++
 			}
 		}
 	}
+
+	gammaResult := (string(gamma(zeroes, ones)))
+
+	log.Println("Gamma is:", gammaResult)
+	log.Println("Epsilon is:", epsilon(zeroes, ones))
+
+}
+func gamma(zeroes []int, ones []int) []byte {
+
+	gamma := make([]byte, 5)
+
+	for i := 0; i < 5; i++ {
+		if zeroes[i] < ones[i] {
+			gamma[i] = '1'
+		} else {
+			gamma[i] = '0'
+		}
+	}
+
+	return gamma
+}
+
+func epsilon(zeroes []int, ones []int) []byte {
+	epsilon := gamma(zeroes, ones) // hehe
+
+	for i, x := range epsilon {
+		if x == '1' {
+			epsilon[i] = '0'
+		}
+		if x == '0' {
+			epsilon[i] = '1'
+		}
+	} // this maybe wasn't the smartest way to do this. but i thought it was a little clever
+
+	return epsilon
 }

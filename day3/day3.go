db416071b08aa93ee18c0d48103c9c171c805720
Caleb Smith <caleb@calebsmith.net>
2021-12-03 05:35:41 +0000

    progress, still need to process ones and zeroes to gamma

--- day3/day3.go
+++ day3/day3.go
@@ -7,6 +7,9 @@ import (
 )
 
 func main() {
+
+	input := make([]string, 0)
+
 	file, err := os.Open("day3.demoinput.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -15,9 +18,27 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() { // for every line in the file
-
+		input = append(input, scanner.Text())
 	}
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	log.Println(input)
+
+}
+func gamma(input []string) int {
+	zeroes := make([]int, 5, 5)
+	ones := make([]int, 5, 5)
+
+	for _, entry := range input {
+		for i, bit := range entry {
+			if bit == 48 {
+				zeroes[i]++
+			}
+			if bit == 49 {
+				ones[1]++
+			}
+		}
 	}
 }
